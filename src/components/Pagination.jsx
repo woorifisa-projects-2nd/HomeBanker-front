ad1802eaa440d4e422f34685aa0f1c03cad56204
oozeume <oozeume@gmail.com>
2024-04-01 15:20:17 +0900

    #3 feat: 상담게시판 리스트 조회, 글 처리완료, 글 삭제 기능

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,40 @@
+import React, { Fragment } from 'react'
+import { Button, Flex, HStack } from "@chakra-ui/react"
+
+export default function Pagination({ pagination, setPagination }) {
+  const { totalItems, itemCountPerPage, pageCount, currentPage } = pagination;
+
+  const totalPages = Math.ceil(totalItems / itemCountPerPage);
+  const noPrev = currentPage === 0;
+  const noNext = currentPage + pageCount - 1 >= totalPages;
+
+  return (
+    <Flex width={"100%"} >
+      <Flex justifyContent={"center"} alignItems={"center"} width={"100%"}>{!noPrev &&
+        <Button variant={"none"} bgColor="white" onClick={
+          () => {
+            setPagination({ ...pagination, currentPage: currentPage - 1 })
+          }}>이전</Button>}
+
+        <HStack>
+          {[...Array(pageCount)].map((a, i) => (
+            <Fragment key={i}>
+              {currentPage + i <= totalPages && (
+                <Button onClick={() =>
+                  setPagination({ ...pagination, currentPage: i })
+                } bgColor={currentPage === i ? "blue400" : "white"}>{i + 1}</Button>
+              )}
+            </Fragment>
+          ))}
+        </HStack>
+
+        {!noNext &&
+          <Button variant={"none"} bgColor="white" onClick={() =>
+            setPagination({ totalItems, itemCountPerPage, pageCount, currentPage: currentPage + 1 })
+          }>다음</Button>}
+      </Flex>
+    </Flex>
+
+
+  )
+}
--- src/pages/board/index.jsx
+++ src/pages/board/index.jsx
@@ -1,7 +1,148 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '../../api/api';
+import React, { useEffect, useState } from 'react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Spinner,
+  Switch,
+  Button,
+  useToast
+} from '@chakra-ui/react'
+import Pagination from '../../components/Pagination';
+
+const PAGINATION_SIZE = 5;
 
 export default function Board() {
+  const toast = useToast()
+  const [boards, setboards] = useState([])
+  const [pagination, setPagination] = useState({
+    totalItems: 0,
+    itemCountPerPage: 0,
+    pageCount: 0,
+    currentPage: 0
+  })
+
+  // 상담게시글 리스트 조회
+  const { data, isLoading, refetch: refetchBoards } = useQuery({
+    queryKey: ['boards', pagination.currentPage],
+    queryFn: () => api.get(`/api/board?size=${PAGINATION_SIZE}&page=${pagination.currentPage}`),
+    keepPreviousData: true,
+  })
+
+  // 상담 처리 완료 API
+  const changeBoardStatus = (boardId) => {
+    api.put(`/api/banker/board/${boardId}`).then(() => {
+      refetchBoards()
+      toast({
+        position: 'top',
+        title: '처리 완료 되었습니다.',
+        status: 'success',
+        duration: 800,
+        isClosable: true,
+      })
+    })
+  }
+
+  // 삭제 API
+  const deleteBoard = (boardId) => {
+    api.delete(`/api/banker/board/${boardId}`).then(() => {
+      refetchBoards()
+      toast({
+        position: 'top',
+        title: '삭제 되었습니다.',
+        status: 'success',
+        duration: 800,
+        isClosable: true,
+      })
+    }
+    )
+  }
+
+  useEffect(() => {
+    if (data) {
+      setboards(data.data.boardItems);
+      setPagination({
+        totalItems: data.data.pagination.totalElements,
+        itemCountPerPage: PAGINATION_SIZE,
+        pageCount: data.data.pagination.totalPages,
+        currentPage: data.data.pagination.pageNumber
+      })
+    }
+  }, [data])
+
+  useEffect(() => {
+    refetchBoards()
+  }, [pagination.currentPage])
+
   return (
-    <div>상담게시판</div>
+    <>
+      <div>상담게시판</div>
+      {isLoading ?
+        <Spinner />
+        :
+        <>
+          {boards &&
+            <TableContainer>
+              <Table variant='simple'>
+                {/* 테이블 헤더 */}
+                <Thead>
+                  <Tr>
+                    <Th>내용</Th>
+                    <Th>등록일</Th>
+                    <Th>유선 회신 여부</Th>
+                    <Th>담당자 이름</Th>
+                    <Th>처리일</Th>
+                    <Th>처리 완료</Th>
+                    <Th>노출 여부</Th>
+                  </Tr>
+                </Thead>
+
+                {/* 테이블 바디 */}
+                <Tbody>
+                  {boards.map(item => {
+                    return <Tr key={item.boardId}>
+                      <Td noOfLines={1}>{item.content}</Td>
+                      <Td>{item.createdAt}</Td>
+                      <Td >{item.replyYN}</Td>
+                      <Td >{item.customerName}</Td>
+                      <Td >{item.updatedAt}</Td>
+                      <Td>
+                        <Button
+                          bgColor={item.replyYN === "Y" ? "gray" : "blue"}
+                          onClick={() => changeBoardStatus(item.boardId)}
+                          isDisabled={item.replyYN === "Y"}>처리완료
+                        </Button>
+                        <Button bgColor={"red"}
+                          onClick={() => deleteBoard(item.boardId)}
+                        >
+                          삭제
+                        </Button>
+                      </Td>
+                      <Td><Switch size='lg' /></Td>
+                    </Tr>
+                  })}
+                </Tbody>
+
+              </Table>
+            </TableContainer>
+          }
+
+          {/* 페이지네이션 */}
+          {pagination.totalItems > 0 &&
+            <Pagination pagination={pagination}
+              setPagination={setPagination}
+              totalItems={pagination.totalItems}
+              itemCountPerPage={pagination.itemCountPerPage}
+              pageCount={pagination.pageCount}
+              currentPage={pagination.currentPage}
+            />}
+        </>}
+    </>
   )
 }
