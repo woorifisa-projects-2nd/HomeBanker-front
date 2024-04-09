dc1dc045e7858d22c3f4f2fa763691457e3dd4a0
Kyongryul Na <skrudfbf0813@naver.com>
2024-04-09 16:09:26 +0900

    c

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 import colors from './style/theme.js'
+// import CookiesProvider from '../node_modules/react-cookie/cjs/CookiesProvider.js'
 
 const theme = extendTheme({ colors })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider theme={theme}>
-    <App />
-  </ChakraProvider>
-
+  // <CookiesProvider>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  // </CookiesProvider>
 )
