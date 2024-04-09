cbe3920da942c8bf01b71ebd6493242103973ac3
Kyongryul Na <skrudfbf0813@naver.com>
2024-04-09 10:30:36 +0900

    #1 feat: 로그인 페이지

--- src/pages/board/index.jsx
+++ src/pages/board/index.jsx
@@ -1,7 +1,96 @@
-import React from 'react'
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import {
+  Tabs, TabList, TabPanels, Tab, TabPanel, Box, Flex,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from '@chakra-ui/react'
 
 export default function Board() {
   return (
-    <div>상담게시판</div>
-  )
+    <Box borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Tabs variant="soft-rounded" colorScheme="green">
+        <TabList>
+          <Tab>고객상담 게시판 관리</Tab>
+          <Tab>서비스 관리</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Flex justifyContent="space-around">
+              <Flex flexDirection="column">
+                <span>등록일</span>
+                <span>2024-03-29</span>
+                <span>2024-03-30</span>
+              </Flex>
+              <Flex flexDirection="column">
+                <span>답변여부</span>
+                <span>X</span>
+                <span>X</span>
+              </Flex>
+              <Flex flexDirection="column">
+                <span>내용(앞에서 10글자만)</span>
+                <span>안녕하세요, 고객님. 제가 장기간 궁금해하시던 내용에 대해서 설명드리겠습니다.</span>
+                <span>질문이 있어서 남깁니다.</span>
+              </Flex>
+              <Flex flexDirection="column">
+                <span>고객이름</span>
+                <span>홍길동</span>
+                <span>둘리</span>
+              </Flex>
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <Flex justifyContent="space-around">
+              <Flex flexDirection="column">
+                <Menu>
+                  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                    Actions
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>전체</MenuItem>
+                    <MenuItem>카드</MenuItem>
+                    <MenuItem>예/적금</MenuItem>
+                    <MenuItem>대출</MenuItem>
+                  </MenuList>
+                </Menu>
+                <span>카드</span>
+                <span>등록일</span>
+              </Flex>
+              <Flex flexDirection="column">
+                <span>등록일</span>
+                <span>2024-03-29</span>
+                <span>2024-03-30</span>
+              </Flex>
+              <Flex flexDirection="column">
+                <span>상품이름</span>
+                <span>나라사랑카드</span>
+                <span>전세자금대출</span>
+              </Flex>
+              <Flex flexDirection="column">
+                <span>상품설명(앞에서 10글자만)</span>
+                <span>안녕하세요, 고객님. 제가 장기간 궁금해하시던 내용에 대해서 설명드리겠습니다.</span>
+                <span>질문이 있어서 남깁니다.</span>
+              </Flex>
+              <Flex flexDirection="column">
+                <span>금리</span>
+                <span>홍길동</span>
+                <span>둘리</span>
+              </Flex>
+              <Flex flexDirection="column">
+                <span>노출여부</span>
+                <span>홍길동</span>
+                <span>둘리</span>
+              </Flex>
+            </Flex>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 export default function Login() {
+  const formBackground = useColorModeValue("gray.100", "gray.700");
+
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleId = (e) => {
+    setId(e.target.value);
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:8080/api/login', {
+        id: id,
+        password: password
+      });
+      console.log("로그인 성공!", response.data);
+    } catch (error) {
+      console.error("로그인 실패!", error);
+    }
+  };
   return (
-    <div>로그인</div>
-  )
-}
+    <Flex h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        flexDirection="column"
+        bg={formBackground}
+        p={20}
+        borderRadius={8}
+        boxShadow="lg"
+      >
+        <Heading mb={8}>고객 로그인 페이지</Heading>
+        <Input
+          id="email"
+          placeholder="아이디를 입력하세요"
+          type="email"
+          variant="filled"
+          mb={3}
+          onChange={handleId}
+        />
+        <Input
+          id="password"
+          placeholder="비밀번호를 입력하세요"
+          type="password"
+          variant="filled"
+          mb={6}
+          onChange={handlePassword}
+        />
+        <Button colorScheme="teal" mb={8} onClick={handleLogin}>
+          로그인
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
