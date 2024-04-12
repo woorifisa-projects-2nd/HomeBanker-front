6a6a63d511606962f5062911f7a19f5224634260
Kyongryul Na <skrudfbf0813@naver.com>
2024-04-12 17:16:30 +0900

    #1 chore: main 파일에서 필요없는 부분 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -8,20 +8,12 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import colors from './style/theme.js'
-// import CookiesProvider from '../node_modules/react-cookie/cjs/CookiesProvider.js'
 
 const theme = extendTheme({ colors })
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-// <<<<<<< HEAD
-//   // <CookiesProvider>
-//     <ChakraProvider theme={theme}>
-//       <App />
-//     </ChakraProvider>
-//   // </CookiesProvider>
-// =======
   <ChakraProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
       <App />
