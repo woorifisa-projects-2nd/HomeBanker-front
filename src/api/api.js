044a53f36bb966811ed1a96711317fe8a8ea1cbc
hellena345 <151507047+hellena345@users.noreply.github.com>
2024-04-12 15:09:57 +0900

    배포된 백엔드 url https로 수정
    배포된 백엔드 url https로 수정

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 export const api = Axios.create({
   //baseURL: `http://localhost:8080`,
-  baseURL: `http://homebank-b.shop:443`,
+  baseURL: `https://homebank-b.shop:443`,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8'
   }
